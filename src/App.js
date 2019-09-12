96dfd5b7abe8eaba07e2a4d294ab590362809819
handy <handynugraha29@gmail.com>
2019-09-12 11:06:16 +0700

    menambahkan collection preview, menambahkan data shop dummy. Mengubah directory, menu-item

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
-class App extends React.Component{
-  render(){
-    return(
-      <div>
-        <HomePage />
-      </div>
-    );
-  }
+function App() {
+	return (
+		<div>
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/shop" component={ShopPage} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
